c42c65d292f932c6202702687193d995fa56b9a0
evan0703 <995713297@qq.com>
2018-07-07 15:37:41 +0800

    First commit.

---
+++ app/src/main/java/com/example/providertest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.providertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private String newId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.example.SharedPreferences.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name","A clash of kings");
+                values.put("author","Geroge Martin");
+                values.put("pages",1040);
+                values.put("price",22.85);
+                Uri newUri = getContentResolver().insert(uri,values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.example.SharedPreferences.provider/book");
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is " + name);
+                        Log.d("MainActivity","book author is " + author);
+                        Log.d("MainActivity","book pages is " + pages);
+                        Log.d("MainActivity","book price is " + price);
+                    } cursor.close();
+                }
+            }
+        });
+    }
+}
